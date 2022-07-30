fcaec1e94c4b7216b3121ea47bc91904b132089d
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-07-30 13:01:12 +0000

    picasso: Set xiaomi thermal data path
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- device.mk
+++ device.mk
@@ -513,6 +513,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Thermal
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.sys.thermal.data.path=/data/vendor/thermal/
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
