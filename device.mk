f2bfdf7f42f9a81a0d7bab9ac4364c46a159cf5c
MangoTM <mango@mangobuild.com>
2022-08-13 16:58:37 +0300

    picasso: build android.hardware.drm@1.4.vendor

--- device.mk
+++ device.mk
@@ -233,6 +233,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
